e099e9f9bfc307c4da1e1dfc97149061dbe60cd1
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2025-09-21 22:11:57 +0200

    Add Koa server with basic /hello route
    Introduced Koa and @koa/router dependencies and set up a basic Koa server in backend/src/index.ts with a /hello endpoint. Updated package.json to include necessary dependencies and devDependencies for Koa and TypeScript support.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+import Koa, { Context, Next } from 'koa';
+import Router from '@koa/router';
+
+
+
+
+const app = new Koa();
+
+const router = new Router();
+
+router.get("/hello", (ctx: Context) => {
+    ctx.body = "hello";
+})
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(3000);
