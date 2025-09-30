4c562611cf38e126f725143b2664f8f465b915ae
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2025-10-01 01:46:17 +0200

    Refactor app entry
    Moved app.js to app/index.js and updated index.html to reference the new path.

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -1,6 +1,6 @@
 import { render } from 'preact';
 import { html } from 'htm/preact';
 
-const app = html`<h1>Hello, World!</h1>`;
+const app = () => html`<h1>Hello, World!</h1>`;
 
 render(app, document.body);
