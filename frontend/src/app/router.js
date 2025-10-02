2cdf6f03ee32bc2a434119874071aa1d40b9ecd3
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2025-10-02 14:41:30 +0200

    Add NotFoundPage and update navigation markup
    Introduces NotFoundPage as the default route in the router and refactors the navigation markup to use a list for better structure and accessibility.

--- frontend/src/app/router.js
+++ frontend/src/app/router.js
@@ -3,23 +3,27 @@ import { Router } from "preact-router";
 import HomePage from "./pages/home";
 import { Fragment } from "preact";
 import LoginPage from "./pages/login";
+import NotFoundPage from "./pages/not-found";
 
 const AppRouter = () => html`
   <${Fragment}>
     <header>
-      <hgroup>
-        <h1>Fake Restaurants</h1>
-        <p>The Nonciclopedia for Restaurants</p>
-      </hgroup>
       <nav>
-        <a href="/">Home</a>
-        <a href="/login">Log in</a>
+        <hgroup>
+          <h1>Fake Restaurants</h1>
+          <p>The Nonciclopedia for Restaurants</p>
+        </hgroup>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/login">Log in</a></li>
+        </ul>
       </nav>
     </header>
     <main>
       <${Router}>
         <${HomePage} path="/" />
         <${LoginPage} path="/login" />
+        <${NotFoundPage} default />
       <//>
     </main>
   <//>
