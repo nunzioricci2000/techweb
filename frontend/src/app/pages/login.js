d8abdfaaf4904e16d91ce4ef9b04f605e01cdc4d
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2025-10-02 14:42:17 +0200

    Add client-side validation to login form
    Introduces username and password validation with error messages and aria-invalid attributes for accessibility. The form now prevents submission if either field is less than 8 characters, improving user feedback and input handling.

--- frontend/src/app/pages/login.js
+++ frontend/src/app/pages/login.js
@@ -1,32 +1,81 @@
 import { html } from "htm/preact";
+import { signal, computed, batch } from "@preact/signals-core";
 
-const LoginPage = () => {
-  return html`
-    <h2>Log in</h2>
-    <form>
-      <fieldset>
-        <label for="username">Username</label>
-        <input
-          id="username"
-          name="username"
-          placeholder="YourAwesomeUsername"
-          autocomplete="username"
-        />
-        <label>Password</label>
-        <input
-          name="password"
-          placeholder="YourSuperSecretPassword"
-          autocomplete="current-password"
-        />
-      </fieldset>
-      <input type="submit" value="Log in" />
-      <!--
-    <small
-      >You don't have an account yet? <a href="/register">Register!</a></small
-    >
-    -->
-    </form>
-  `;
+const usernameError = signal("");
+const passwordError = signal("");
+
+/**
+ * showUsernameError is strangely typed as true | null
+ * because aria-invalid behaves differently if set to
+ * false or not set at all
+ */
+const showUsernameError = computed(() => {
+  return usernameError.value !== "" ? true : null;
+});
+/** @see showUsernameError */
+const showPasswordError = computed(() => {
+  return passwordError.value !== "" ? true : null;
+});
+
+/**
+ * @param {SubmitEvent} e
+ */
+const onSubmit = async (e) => {
+  e.preventDefault();
+  const data = new FormData(e.currentTarget);
+  const username = data.get("username");
+  const password = data.get("password");
+  let isInputValidated = true;
+  batch(() => {
+    usernameError.value = "";
+    passwordError.value = "";
+    if (username.length < 8) {
+      usernameError.value = "The username must be 8 characters at least!";
+      isInputValidated = false;
+    }
+    if (password.length < 8) {
+      passwordError.value = "The password must be 8 characters at least!";
+      isInputValidated = false;
+    }
+  });
+  if (!isInputValidated) return;
+  console.log("logged!");
 };
 
-export default LoginPage;
+export default function LoginPage() {
+  return html`
+    <h2>Log in</h2>
+    <form onSubmit=${onSubmit}>
+      <fieldset>
+        <label for="username">
+          Username
+          <input
+            id="username"
+            name="username"
+            placeholder="YourAwesomeUsername"
+            autocomplete="username"
+            aria-invalid="${showUsernameError}"
+          />
+          ${showUsernameError.value &&
+          html`<small>${usernameError.value}</small>`}
+        </label>
+        <label>
+          Password
+          <input
+            name="password"
+            placeholder="YourSuperSecretPassword"
+            autocomplete="current-password"
+            type="password"
+            aria-invalid="${showPasswordError}"
+          />
+          ${showPasswordError.value &&
+          html`<small>${passwordError.value}</small>`}
+        </label>
+      </fieldset>
+      <input type="submit" value="Log in" />
+      <small
+        >You don't have an account yet? <a href="/register">Register!</a></small
+      >
+    </form>
+  `;
+}
