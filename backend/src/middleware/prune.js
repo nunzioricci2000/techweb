4c177a295f7d0fbf30b71d95a357144b333287ef
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2025-10-14 09:46:24 +0200

    Add prune middleware for response validation
    Introduces a middleware that validates and prunes response bodies using a Joi schema. If the response does not conform to the schema, it returns a 500 error with a relevant message.

---
+++ backend/src/middleware/prune.js
@@ -0,0 +1,24 @@
+/**
+ * @template {T}
+ * @param {import("joi").Schema<T>} schema
+ * @returns {import("koa").Middleware<AppState>}
+ */
+export default function prune(schema) {
+  return async (ctx, next) => {
+    await next();
+    if (ctx.status >= 400) {
+      return;
+    }
+    const data = ctx.response.body;
+    const valid = schema.validate(data, {
+      abortEarly: false,
+      stripUnknown: true,
+    });
+    if (valid.error) {
+      console.debug("Respone not valid:", valid.value);
+      ctx.status = 500;
+      ctx.body = { error: valid.error?.message ?? "Internal Error!" };
+      return;
+    }
+  };
+}
