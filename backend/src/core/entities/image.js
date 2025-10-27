0f8115e82b05ec1fbcad6f8d628334412df6ab4c
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2025-10-28 00:05:53 +0100

    Add Image entity class
    Introduces an Image class with a private URL property and validation in the constructor. Provides a getter for accessing the image URL.

---
+++ backend/src/core/entities/image.js
@@ -0,0 +1,24 @@
+export default class Image {
+  /** @type {string} */
+  #url;
+
+  /**
+   * Get the URL of the image
+   * @returns {string} Image URL
+   */
+  get url() {
+    return this.#url;
+  }
+
+  /**
+   * Image entity
+   * @param {object} params
+   * @param {string} params.url - URL of the image
+   */
+  constructor({ url }) {
+    if (typeof url !== "string") {
+      throw new TypeError("Image URL must be a string");
+    }
+    this.#url = url;
+  }
+}
